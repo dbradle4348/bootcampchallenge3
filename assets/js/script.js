664b0d86f069ddd32723a6e479fd74e10447c0f5
King Julian <daniel.bradley09@gmail.com>
2020-11-15 22:27:17 -0700

    confirm character questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,59 @@
-// Assignment code here
-
 
 // Get references to the #generate element
+
+var passwordText = document.querySelector("#password");
+
 var generateBtn = document.querySelector("#generate");
+
+
+// Assignment code here
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "123456789";
+const specialCharacters = "!@#$%^&*()[];',./<>?:{}+`~";
+ var password = writePassword();
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
+  // lower case characters
+  var passLengthQuestion = window.prompt("How many characters would you like to use for the password?");
+    
+    if (Number(passLengthQuestion < 8) | Number(passLengthQuestion > 128)) {
+      alert("Password must be between 8 and 128 characters in length!");
+      return;
+    }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    // lower case characters
+
+    var lowerQuestion = confirm("Would you like to include lower case characters?");
+
+    if (lowerQuestion) {
+      password += lowerQuestion;
+    }
+
+    // upper case characters
+  
+    var upperQuestion = confirm("Would you like to include upper case characters?");
+
+    // number characters
+
+    var numQuestion = confirm("Would you like to include numbers in your password?")
+
+    // special characters
+
+    var specQuestion = confirm("Almost done! Would you like to include special characters in you password?")
+
+
+
+
+
+
+
+
